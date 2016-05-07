71027cd01557262cfb869f1a128c27969aac1acd
weirdNox <goncalossantos98@gmail.com>
2016-05-07 19:52:10 +0100

    Revert to JS parallax

--- main.js
+++ main.js
@@ -13,8 +13,79 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-        document.body.classList.add("ios");
-        document.documentElement.classList.add("ios");
+    function detectIe() {
+        var ua = window.navigator.userAgent;
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        return false;
+    }
+
+    var lastScroll = 0;
+    var ticking = false;
+    images = document.querySelectorAll(".parallax img");
+    var pageHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight,
+                              document.documentElement.clientHeight, document.documentElement.scrollHeight,
+                              document.documentElement.offsetHeight);
+    var windowHeight = window.innerHeight;
+
+    function parallax(scroll) {
+        scroll = Math.max(0, Math.min(pageHeight - windowHeight, scroll));
+
+        for (i = 0; i < images.length; ++i) {
+            var img = images[i];
+            var div = img.parentElement;
+
+            img.style.top = "0";
+
+            var containerHeight = div.offsetHeight;
+            var rect = div.getBoundingClientRect();
+            var top = rect.top + scroll;
+            var bottom = top + containerHeight;
+
+            if ((bottom > scroll) && (top < (scroll + windowHeight))) {
+                var imgHeight = img.offsetHeight;
+
+                var parallaxDist = imgHeight - containerHeight;
+
+                var windowBottom = scroll + windowHeight;
+                var percentScrolled = (windowBottom - top) / (containerHeight + windowHeight);
+                var parallax = Math.round((parallaxDist * percentScrolled));
+
+                img.style.transform = "translate3D(-50%," + (containerHeight + parallax - imgHeight) + "px, 0)";
+            }
+        }
+    }
+
+    function requestParallax() {
+        lastScroll = window.scrollY;
+        if (!ticking) {
+            ticking = true;
+            window.requestAnimationFrame(function() {
+                parallax(lastScroll);
+                ticking = false;
+            });
+        }
+    }
+
+    if(!detectIe())
+    {
+        window.addEventListener('scroll', requestParallax);
+        window.addEventListener('resize', requestParallax);
+        requestParallax();
     }
 });
